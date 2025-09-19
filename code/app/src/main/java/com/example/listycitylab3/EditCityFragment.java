c82ba917f9976ce468e6ebf71f8fe7c2071b405e
reschan <minhtrung.06.13@gmail.com>
2025-09-18 18:39:20 -0600

    Add files via upload

---
+++ code/app/src/main/java/com/example/listycitylab3/EditCityFragment.java
@@ -0,0 +1,54 @@
+package com.example.listycitylab3;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class EditCityFragment extends DialogFragment {
+    interface EditCityDialogListener {
+        void editCity(String city, String prov, int pos);
+    }
+    private EditCityDialogListener listener;
+    public int pos;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof EditCityFragment.EditCityDialogListener) {
+            listener = (EditCityFragment.EditCityDialogListener) context;
+        } else {
+            throw new RuntimeException(context + " must implement EditCityDialogListener");
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view =
+                LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        return builder
+                .setView(view)
+                .setTitle("Edit city")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Add", (dialog, which) -> {
+                    String cityName = editCityName.getText().toString();
+                    String provinceName = editProvinceName.getText().toString();
+                    listener.editCity(cityName, provinceName, pos);
+                })
+                .create();
+    }
+}
